64413e12cf48b2440f1c48deaf5c40de6fb7107a
thousif <thousif20@gmail.com>
2015-12-08 07:17:09 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+<head> <meta charset="utf-8"> </head>
+<body><form action="search-tweets.php" method="post">
+search: <input type="text" name="word">
+<input type="submit">
+</form>
+</body>
+</html>
